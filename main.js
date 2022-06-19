8a5b514c4e40feb9a4fd3cd703ad4cf80723e9e4
StephenHenryJr <shenryjr_85@hotmail.com>
2022-06-19 12:20:03 -0400

    Changed layout and centered container

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ function validateCred() {
   let str = input.value;
   let num = parseFloat(str);
   let numArr = [...String(num)].map(Number);
-  console.log(numArr)
   let total = 0;
   for (let i = numArr.length - 1; i >= 0; i--) {
     let currValue = numArr[i]
@@ -57,44 +56,35 @@ button.addEventListener('click', validateCred)
 //   return invalid;
 // }
 
-// // Test function
-// console.log(findInvalidCards([valid1, valid2, valid3, valid4, valid5]));// Shouldn't print anything
-// console.log(findInvalidCards([invalid1, invalid2, invalid3, invalid4, invalid5])); // Should print all of the numbers
+function idInvalidCardCompanies(invalidBatch) {
+  const companies = [];
+  for (let i = 0; i < invalidBatch.length; i++) {
+    switch (invalidBatch[i][0]) {
+      case 3:
+        if (companies.indexOf('Amex') === -1) {
+          companies.push('Amex');
+        }
+        break
+      case 4:
+        if (companies.indexOf('Visa') === -1) {
+          companies.push('Visa');
+        }
+        break
+      case 5:
+        if (companies.indexOf('Mastercard') === -1) {
+          companies.push('Mastercard');
+        }
+        break
+      case 6:
+        if (companies.indexOf('Discover') === -1) {
+          companies.push('Discover');
+        }
+        break
+      default:
+        console.log('Company not found');
+    }
+  }
+  return companies;
+}
 
-// console.log(findInvalidCards(batch)); // Test what the mystery numbers are
-
-// function idInvalidCardCompanies(invalidBatch) {
-//   const companies = [];
-//   for (let i = 0; i < invalidBatch.length; i++) {
-//     switch (invalidBatch[i][0]) {
-//       case 3:
-//         if (companies.indexOf('Amex') === -1) {
-//           companies.push('Amex');
-//         }
-//         break
-//       case 4:
-//         if (companies.indexOf('Visa') === -1) {
-//           companies.push('Visa');
-//         }
-//         break
-//       case 5:
-//         if (companies.indexOf('Mastercard') === -1) {
-//           companies.push('Mastercard');
-//         }
-//         break
-//       case 6:
-//         if (companies.indexOf('Discover') === -1) {
-//           companies.push('Discover');
-//         }
-//         break
-//       default:
-//         console.log('Company not found');
-//     }
-//   }
-//   return companies;
-// }
-
-// console.log(idInvalidCardCompanies([invalid1])); // Should print['visa']
-// console.log(idInvalidCardCompanies([invalid2])); // Should print ['mastercard']
-// console.log(idInvalidCardCompanies(batch)); // Find out which companies have mailed out invalid cards
 
